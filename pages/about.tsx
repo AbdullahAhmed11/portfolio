12ef5fd732ec9f10bde622e5bfcccab642b975cb
Abdullahahmed11 <abdullah77zzx@gmail.com>
2023-07-02 07:40:01 +0300

    newRouter

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,44 @@
+import NavBar from "@/componets/Nav/NabBar";
 import React from "react";
+import { Box, Text } from "@chakra-ui/react"
+import About from "@/componets/About/About"
+import AboutMobile from "@/componets/About/AboutMobile"
+import { useMediaQuery } from '@chakra-ui/react'
+import MaxWidthWrapper from "@/componets/MaxWidthWrapper/MaxWidthWrapper";
+import Link from "next/link";
+import Head from "next/head";
 
 function AboutPage () {
+    const [isLargerThan798] = useMediaQuery('(min-width: 798px)')
+
     return (
         <>
-            <h1>Hiiiiidddd</h1>
+        <Head>
+            <title>@devAbdullah | About</title>
+        </Head>
+        <MaxWidthWrapper>
+
+            <Box
+                mt="40px"
+                display="flex"
+                gap="10px"
+            >
+                <Text>Back to</Text>
+                    <Text
+                        as="a"
+                        textDecorationLine="none"
+                        fontWeight="bold"
+                        href="/"
+                        color="#000"
+                    >Home</Text>
+            </Box>
+        </MaxWidthWrapper>
+
+            <Box mt="40px">
+                {
+                isLargerThan798 ? <About/> : <AboutMobile/>
+                }
+            </Box>
         </>
     )
 }
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,14 +1,36 @@
 import React from "react";
-import { Box } from "@chakra-ui/react"
+import { Box,Text } from "@chakra-ui/react"
 import { useMediaQuery } from '@chakra-ui/react'
 import Projects from "@/componets/Projects/Projects"
 import NavBar from "@/componets/Nav/NabBar"
 import MobileNavBar from "@/componets/Nav/MobileNavBar"
+import Head from "next/head";
+import MaxWidthWrapper from "@/componets/MaxWidthWrapper";
 
 function ProjectsPage () {
       const [isLargerThan798] = useMediaQuery('(min-width: 798px)')
     return (
     <>
+        <Head>
+            <title>@devAbdullah | Projects</title>
+        </Head>
+        <MaxWidthWrapper>
+
+            <Box
+                mt="40px"
+                display="flex"
+                gap="10px"
+            >
+                <Text>Back to</Text>
+                    <Text
+                        as="a"
+                        textDecorationLine="none"
+                        fontWeight="bold"
+                        href="/"
+                        color="#000"
+                    >Home</Text>
+            </Box>
+        </MaxWidthWrapper>
         <Box>
             <Projects/>
         </Box> 
